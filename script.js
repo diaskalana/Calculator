935db44990ef2e7ac9c3f9ba1c1485db3f56bdfa
diaskalana <74653324+diaskalana@users.noreply.github.com>
2024-01-01 20:23:25 +0530

    Add clear and delete features

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const year = document.querySelector('.year')
 const calScreen = document.querySelector('.cal-screen > p')
 const regularButtons = document.querySelector('.regular-buttons')
+const clearBtn = document.querySelector('#clearBtn')
+const deleteBtn = document.querySelector('#deleteBtn')
 
 year.textContent = new Date().getFullYear();
 calScreen.textContent = 0;
@@ -10,29 +12,35 @@ function addToScreen(array) {
     calScreen.textContent = array.join('')
 }
 
+deleteBtn.addEventListener('click', () => {
+    if (numSet.length == 1) {
+        numSet[0] = 0
+        addToScreen(numSet)
+
+    }
+    if (calScreen.textContent != 0) {
+        numSet.pop()
+    }
+    addToScreen(numSet)
+})
+
+clearBtn.addEventListener('click', () => {
+    numSet.splice(0, numSet.length)
+    numSet[0] = 0
+    addToScreen(numSet)
+})
+
+
 regularButtons.addEventListener('click', (event) => {
+
     if (numSet.length < 8) {
-        if (event.target.id === 'num0') {
-            numSet.push(0)
-        } else if (event.target.id === 'num1') {
-            numSet.push(1)
-        } else if (event.target.id === 'num2') {
-            numSet.push(2)
-        } else if (event.target.id === 'num3') {
-            numSet.push(3)
-        } else if (event.target.id === 'num4') {
-            numSet.push(4)
-        } else if (event.target.id === 'num5') {
-            numSet.push(5)
-        } else if (event.target.id === 'num6') {
-            numSet.push(6)
-        } else if (event.target.id === 'num7') {
-            numSet.push(7)
-        } else if (event.target.id === 'num8') {
-            numSet.push(8)
-        } else if (event.target.id === 'num9') {
-            numSet.push(9)
-        } else if (event.target.id === 'period') {
+        if (event.target.id.startsWith('num')) {
+            if (numSet[0] == 0) {
+                numSet.pop()
+            }
+            numSet.push(event.target.textContent)
+        }
+        if (event.target.id === 'period') {
             if (!numSet.includes('.')) {
                 numSet.push('.')
             }
