563cb0b42fa0ece5f9c8efde5bf743881ef029fa
diaskalana <74653324+diaskalana@users.noreply.github.com>
2024-01-01 22:15:13 +0530

    Add basic functionalities

--- script.js
+++ script.js
@@ -7,11 +7,32 @@ const deleteBtn = document.querySelector('#deleteBtn')
 year.textContent = new Date().getFullYear();
 calScreen.textContent = 0;
 const numSet = []
+let numSetTwo = []
+let result;
+let operation;
 
+// To display to screen
 function addToScreen(array) {
     calScreen.textContent = array.join('')
 }
 
+function convertToNumber(array) {
+    return Number(array.join(''))
+}
+
+function displayResult(res) {
+    res = String(res)
+    if (res.length > 9) {
+        alert("Note that only the first 9 or 8 characters are shown!")
+        res = res.substring(0, 9)
+        if (res.endsWith('.')) {
+            res = res.substring(0, 8)
+        }
+    }
+    calScreen.textContent = res;
+}
+
+// To enable delete function
 deleteBtn.addEventListener('click', () => {
     if (numSet.length == 1) {
         numSet[0] = 0
@@ -24,13 +45,14 @@ deleteBtn.addEventListener('click', () => {
     addToScreen(numSet)
 })
 
+// To enable clear function
 clearBtn.addEventListener('click', () => {
     numSet.splice(0, numSet.length)
     numSet[0] = 0
     addToScreen(numSet)
 })
 
-
+// To capture number pad events
 regularButtons.addEventListener('click', (event) => {
 
     if (numSet.length < 8) {
@@ -45,9 +67,84 @@ regularButtons.addEventListener('click', (event) => {
                 numSet.push('.')
             }
         }
-    } else {
+    }
+    else {
         alert("The maximum input limit has been reached!")
     }
     addToScreen(numSet)
+
+    if (event.target.id === 'divide') {
+
+        if (numSet[0] != 0) {
+            numSetTwo = [...numSet]
+            numSet.splice(0, numSet.length)
+            numSet[0] = 0
+            addToScreen(numSet)
+            // console.log('check')
+            operation = 'divide'
+        }
+
+    }
+
+    if (event.target.id === 'multiply') {
+
+        if (numSet[0] != 0) {
+            numSetTwo = [...numSet]
+            numSet.splice(0, numSet.length)
+            numSet[0] = 0
+            addToScreen(numSet)
+            operation = 'multiply'
+        }
+
+    }
+
+    if (event.target.id === 'plus') {
+
+        if (numSet[0] != 0) {
+            numSetTwo = [...numSet]
+            numSet.splice(0, numSet.length)
+            numSet[0] = 0
+            addToScreen(numSet)
+            operation = 'plus'
+        }
+
+    }
+
+    if (event.target.id === 'minus') {
+
+        if (numSet[0] != 0) {
+            numSetTwo = [...numSet]
+            numSet.splice(0, numSet.length)
+            numSet[0] = 0
+            addToScreen(numSet)
+            operation = 'minus'
+        }
+
+    }
+
+    if (event.target.id === 'equal') {
+        if (numSet[0] != 0) {
+            if (operation == 'divide') {
+                result = convertToNumber(numSetTwo) / convertToNumber(numSet)
+                // console.log(convertToNumber(numSetTwo), convertToNumber(numSet))
+                displayResult(result)
+            }
+        } else {
+            alert("Can't divide by 0!")
+        }
+
+        if (operation == 'multiply') {
+            result = convertToNumber(numSetTwo) * convertToNumber(numSet)
+            displayResult(result)
+
+        } else if (operation == 'plus') {
+            result = convertToNumber(numSetTwo) + convertToNumber(numSet)
+            displayResult(result)
+        } else if (operation == 'minus') {
+            result = convertToNumber(numSetTwo) - convertToNumber(numSet)
+            displayResult(result)
+        }
+
+    }
 
 })
